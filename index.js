e5c98a375e693fea0bd03ca2b4e51acd2a35f069
zhaoyao91 <zhaoyao91@163.com>
2017-08-20 19:41:34 +0800

    remove pino, update nats

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('checkenv').check()
 
-const logger = require('env-pino')
 const repl = require('repl')
 const connectMethod = require('nats-method')
 const connectEvent = require('nats-event')
@@ -10,13 +9,9 @@ const {NATS_URL, METHOD_NATS_URL, EVENT_NATS_URL} = process.env
 const method = NATS_URL ? connectMethod(NATS_URL) : connectMethod(METHOD_NATS_URL)
 const event = NATS_URL ? connectEvent(NATS_URL) : connectEvent(EVENT_NATS_URL)
 
-method.on('error', err => logger.error(err))
-event.on('error', err => logger.error(err))
-
 const {context} = repl.start('> ')
 context.method = method
 context.event = event
-context.logger = logger
 
 // extend: auto log
 
@@ -28,14 +23,14 @@ if (AUTO_LOG) {
   const originalCall = method.call.bind(method)
   method.call = function (name, input) {
     const id = ++callCount
-    setTimeout(() => logger.info({msg: 'method called', id}), 0)
+    setTimeout(() => console.log('method called:', {id}), 0)
     return originalCall(name, input)
       .then(res => {
-        logger.info({msg: 'method returned', id, result: res})
+        console.log('method returned:', {id, result: res})
         return res
       })
       .catch(err => {
-        logger.error({msg: 'method failed', id, error: err})
+        console.error('method failed:', {id, error: err})
       })
   }
 }
